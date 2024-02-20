cb608125f85d89e24076074fbba947b7308b86f4
VisaganS <visagan.shan@gmail.com>
2024-02-20 05:24:55 -0500

    put request for reviews in progress

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -53,31 +53,45 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-    //Get username and find if any reviews made by user.
-    // const isbn = req.params.isbn;
-    // const review = req.query.review;
-    // const username = req.session.username;
-    // const book = books[isbn];
-
-    // if (!username) {
-    //     return res.status(404).json({ message: "User not found." });
-    // }
-
-    // if (!book) {
-    //     return res.status(404).json({ message: "Book not found." });
-    // }
-
-    // const userReviewIndex = books[isbn].reviews.findIndex(r => r.username === username);
-    // if (userReviewIndex !== -1) {
-    //     // Modify the existing review
-    //     books[isbn].reviews[userReviewIndex].review = review;
-    //     return res.status(200).json({ message: "Review modified successfully." });
-    // } else {
-    //     // Add a new review
-    //     book.reviews.push({ username, review });
-    //     return res.status(200).json({ message: "New review added successfully." });
-    // }
-    return res.status(300).json({ message: "User Hi" })
+    const isbn = req.params.isbn;
+    const username = req.session.username;
+    const reviewText = req.query.review;
+    //function declaration to find review of user
+    const findReviewsByUser = (user, isbn) => {
+        const book = books[isbn];
+        
+        // check if Book 1 exists and if it has any reviews
+        if (book && Object.keys(book.reviews).length > 0) {
+            // check if user has made a review for Book 1
+            if (book.reviews.hasOwnProperty(user)) {
+                return book.reviews[user].review;
+            } else {
+                return "User has not made a review for " + book.title;
+            }
+        } else {
+            return "Book has no reviews or doesn't exist ";
+        }
+    }
+    
+    //return err if invalid username
+    if (!username) {
+        return res.status(404).json({ message: "User not found." });
+    }
+    //return if review query is empty
+    if (!review) {
+        return res.status(404).json({ message: "Review not found." });
+    }
+    //find a review made by the user
+    findReviewsByUser(username, isbn)
+    if (reviewLocation !== -1) {
+        //update existing review
+        books[isbn].reviews[reviewLocation].push(newReview);
+        return res.status(200).json({ message: "Review modified successfully." });
+    } else {
+        // Add a new review
+        book.reviews.push({ username, review });
+        return res.status(200).json({ message: "New review added successfully." });
+    }
 });
 
 module.exports.authenticated = regd_users;
